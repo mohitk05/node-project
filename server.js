b879e4349628d7ddca4330894e8604398516a894
Mohit Karekar <karekar.mohit@gmail.com>
2018-02-01 21:05:05 +0530

    node version

--- server.js
+++ server.js
@@ -49,6 +49,6 @@ app.get('/addnode', (req, res) => {
   });
 })*/
 var app = require('./app');
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('Server is listening on port 3000');
 })
